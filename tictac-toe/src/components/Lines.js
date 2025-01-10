3489a664cd09ededd9d4aca41a3d31516c24b61f
joaovictortvs <tavaressjoaoo09@gmail.com>
2025-01-10 17:10:51 -0300

    Criando lógica do jogo

--- tictac-toe/src/components/Lines.js
+++ tictac-toe/src/components/Lines.js
@@ -2,35 +2,46 @@ import Styles from './Lines.module.css'
 
 function Lines(){
 
-    let column = document.querySelectorAll(".column") // add evento para adição dos símbolos
-    column = [...column]
+    let playerX_time = true
+    let playerO_time = false
 
-    column.addEventListener("click",(evt)=>{
-        console.log(evt.target)
-    })
+    function addMark(caixaClick){
+        let caixa = document.getElementById(caixaClick)
+
+        if(playerX_time === true){
+            caixa.innerHTML = "X"
+            playerO_time = true
+            playerX_time = false
+        } else{
+            caixa.innerHTML = "O"
+            playerO_time = false
+            playerX_time = true
+        }
+
+    }
 
     return(
         <div className={Styles.container_lines}>
             <div className={Styles.lines}>
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="supEsq" onClick={(evt)=>addMark(evt.target.id)}></div>
 
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="supMei" onClick={(evt)=>addMark(evt.target.id)}></div>
 
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="supDir" onClick={(evt)=>addMark(evt.target.id)}></div>
             </div>
             <div className={Styles.lines}> 
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="meiEsq" onClick={(evt)=>addMark(evt.target.id)}></div>
 
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="meiMei" onClick={(evt)=>addMark(evt.target.id)}></div>
 
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="meiDir" onClick={(evt)=>addMark(evt.target.id)}></div>
             </div>
             <div className={Styles.lines}>
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="infEsq" onClick={(evt)=>addMark(evt.target.id)}></div>
 
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="infMei" onClick={(evt)=>addMark(evt.target.id)}></div>
 
-                <div className={Styles.column}></div>
+                <div className={Styles.column} id="infDir" onClick={(evt)=>addMark(evt.target.id)}></div>
             </div>
         </div>
     )   
